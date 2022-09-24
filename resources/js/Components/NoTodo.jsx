743f3ee02c24ca53b9c0ca38cdd9cffc1f2f012d
Fahim Sakib <fsakib991@gmail.com>
2022-09-24 14:27:15 +0600

    notodo message

--- resources/js/Components/NoTodo.jsx
+++ resources/js/Components/NoTodo.jsx
@@ -1,7 +1,16 @@
 export default function Notodo({ filter }) {
-  return (
-    <div className="no-todo-container">
-      <p>No {filter} Todo available! Add new...</p>
-    </div>
-  )
+  const message = () => {
+    if (filter === 'all') {
+      return <p>No todo available! Add new or restore from trash.</p>
+    }
+    if (filter === 'active') {
+      return <p>No active todo available!</p>
+    }
+    if (filter === 'complete') {
+      return <p>No completed todo available!</p>
+    } else {
+      return <p>Your trash is empty!</p>
+    }
+  }
+  return <div className="no-todo-container">{message()}</div>
 }
